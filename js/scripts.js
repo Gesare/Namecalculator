e8f741868c8886d4a740743b5e29d5d3a60edcca
Gesare <gesareayienda@gmail.com>
2020-02-16 19:57:40 +0300

    Added  text and changed css abit.

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@ var female = document.getElementById("female");
 if(day<=0||day>31)
 alert("Please enter the correct day");
 else if(month<0||month>12 ||month==2 &&day>29)
-alert("Please enter the correct day");
+alert("Please enter the correct month");
 var day = new Date(year + "/" + month + "/" + day);
 var birthDay=day.getDay();
 if(male.checked==true){
