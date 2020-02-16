9e0e203517fbd06161a22ca14cc66b15b050f9cf
Gesare <gesareayienda@gmail.com>
2020-02-16 20:03:53 +0300

    Added  text and changed css abit.

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,7 @@ var femaleNames=[
     "Afua",
     "Ama",
 ];
+
 //calling functions
 function getName() {
 var year=parseInt(document.getElementById("year").value);
@@ -32,6 +33,52 @@ var month =parseInt(document.getElementById("month").value);
 var day=parseInt(document.getElementById("day").value);
 var male = document.getElementById("male");
 var female = document.getElementById("female");
+
+
+//validation
+if(day<=0||day>31)
+alert("Please enter the correct day");
+else if(month<0||month>12 ||month==2 &&day>29)
+alert("Please enter the correct month");
+var day = new Date(year + "/" + month + "/" + day);
+var birthDay=day.getDay();
+if(male.checked==true){
+   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay])
+}
+else if(female.checked==true){
+   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleNames[birthDay])
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //validation
