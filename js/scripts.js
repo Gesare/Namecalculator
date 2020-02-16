1e65d56109cc8e0f3ab21dd51eed24f96233bd9e
Gesare <gesareayienda@gmail.com>
2020-02-16 19:33:14 +0300

    added image

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,19 @@ else if(female.checked==true){
    alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleNames[birthDay])
 }
 }
+
+
+//validation
+if(day<=0||day>31)
+alert("Please enter the correct day");
+else if(month<0||month>12 ||month==2 &&day>29)
+alert("Please enter the correct day");
+var day = new Date(year + "/" + month + "/" + day);
+var birthDay=day.getDay();
+if(male.checked==true){
+   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay])
+}
+else if(female.checked==true){
+   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleNames[birthDay])
+}
+}
